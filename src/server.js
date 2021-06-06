9391506fc229cc96738785c076cc608e152ae54f
Vignesh Gawali <vbg3008@gmail.com>
2021-06-06 15:07:23 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-app.use(express.static('./dist/vbgtest'));
+app.use(express.static('./dist/codders-club'));
 
 app.get('/*' , (req , res) => 
-    res.sendFile('index.html' , {root:'dist/vbgtest/'}),
+    res.sendFile('index.html' , {root:'dist/codders-club/'}),
     );
 
 
